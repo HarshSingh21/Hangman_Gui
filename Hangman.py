8ff1c92998edaeeae55189d96aa0af78486fcf88
Harsh Singh <94156692+HarshSingh21@users.noreply.github.com>
2022-11-20 22:24:52 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -4,7 +4,8 @@ from string import ascii_uppercase
 import random
 from tkinter import messagebox
 run = True
-while run:  
+while run: 
+    
     root = Tk()  # creating object to tkinter class (creating a window)
     root.geometry("1050x550")
     root.title('Hangman')
